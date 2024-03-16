1b1faf80c26ca96b52aef1fcfb31ad6707de399a
Tim Zimmermann <tim@linux4.de>
2024-03-16 21:11:31 +0100

    gta4xl-common: Remove RIL interfaces from common tree
    Change-Id: I1d72c3c1cb23c9665ea3ca53bab2251353954675

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -121,7 +121,7 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE := false
 
 ## Manifest
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/device_framework_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
